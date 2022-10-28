c7126de4d4df5f4f4cacefd4682c4b94c88f588c
Mikael Josias <mikaeljosias2002@gmail.com>
2022-10-28 15:41:34 -0300

    feat(index): add new users on the server

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,31 @@ const inputUser = document.querySelector("#inputUser");
 const btnEntrar = document.querySelector(".login__entrar")
 
 let usuario = {
-    nome: "",
+    name: ""
 }
 
 const validarInput = () => {
     if (inputUser.value.trim() !== "" && inputUser.value !== undefined) {
-        
+        usuario.name = inputUser.value;
+
         return true;
     }
 
     return false;
 };
 
+const enviarUsuario = () => {
 
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario)
+    .then(() => {console.log("sucesso")})
+    .catch(() => {console.log("erro")});
+}
 
-btnEntrar.addEventListener("click", validarInput);
+btnEntrar.addEventListener("click", () => {
+    if (validarInput()) {
+        
+        enviarUsuario();
+    }else{
+        alert("Nome de usuário inválido");
+    }
+});
